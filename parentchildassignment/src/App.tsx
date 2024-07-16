d583ca6086158ebd01f2e3750ca3d35bfe984dc1
yamininallakunta_ldp <yamini.nallakunta@zemosolabs.com>
2024-07-16 12:21:19 +0530

    add parentchildassignment

---
+++ parentchildassignment/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SeederPage from './page/SeederPage';
+import Box from '@mui/material/Box';
+
+
+const App: React.FC =() => {
+  return (
+    <Box sx={{border:'5px solid black', height:600, width:250, ml:20, backgroundColor:'#222124',mt:5}}>
+    <div>
+      <SeederPage />
+    </div>
+    </Box>
+  )
+}
+
+export default App;
---
+++ parentchildassignment/src/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette :{
+        primary :{
+            main : '#2D2D30',
+        },
+        secondary :{
+            main :  '#A5A5A6',
+        },
+        success :{
+            main:'#A5A5A6',
+        }
+    }
+    }
+)
+
+export default theme;
